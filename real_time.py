fc9e320f32c49fe6a2e74c9f77fb0b9667dd921f
yexuting <610802397@qq.com>
2025-06-28 09:15:37 +0800

    测试通达信选项

---
+++ real_time.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from mootdx.quotes import Quotes
+
+
+
+def main():
+    client = Quotes.factory(market='std')
+    data = client.quotes(symbol=["002891"])
+    print(data.columns)
+
+    # 打印列名
+    print("字段列表:")
+    print(data.columns.tolist())
+
+    # 打印每只股票的所有字段值
+    for index, row in data.iterrows():
+        print(f"\n股票代码: {row['code']}")
+        for col in data.columns:
+            print(f"{col}: {row[col]}")
+
+if __name__ == '__main__':
+    main()
